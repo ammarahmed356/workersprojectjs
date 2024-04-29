44f37245139918d4d2d2241b81af732d316af7c4
Ammar Ahmed <aahmed@x35qy169l4.lan>
2024-04-28 19:07:16 -0500

    Project 1 Final Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+// src/index.js
+var src_default = {
+  async fetch(request, env, ctx) {
+    if (request.method === 'POST') {
+           return new Response('This is a POST Request', {
+               headers: {
+                   'content-type': 'application/json',
+               },
+           });
+       }
+       else{
+           return new Response('Hello World - this is NOT a POST Request', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+  }
 };
+export {
+  src_default as default
+};
+//# sourceMappingURL=index.js.map
+
